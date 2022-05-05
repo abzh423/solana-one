cacb4804829eecdf8e09c223d5ee0090a1c55d95
Abulkhaiyr <abzh423@protonmail.com>
2022-05-05 09:48:39 +0600

    Typography and css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Counter } from './Counter'
+import './css/reset.css'
+import './css/typography.css'
 
 export const App = () => {
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
+      <blockquote>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, sint!
+      </blockquote>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit quae
+      laborum incidunt magni facere saepe molestias sapiente iusto. Ad
+      blanditiis dolor error similique iste repellendus, harum laborum illum at
+      aperiam!
     </>
   )
 }
