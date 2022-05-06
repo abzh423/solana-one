656f2dae86f4815c4361fff1972c9c6d8a6735f6
Abulkhaiyr <abzh423@protonmail.com>
2022-05-06 08:55:39 +0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import { useEffect } from 'react'
 import './css/reset.css'
 import './css/typography.css'
 
 export const App = () => {
+  const checkConnection = async () => {
+    try {
+      const { solana } = window
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet in the house!')
+        }
+      } else {
+        alert('Solana wallet has not been found!')
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkConnection()
+    }
+    window.addEventListener('load', onLoad)
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
-      <blockquote>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, sint!
-      </blockquote>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit quae
-      laborum incidunt magni facere saepe molestias sapiente iusto. Ad
-      blanditiis dolor error similique iste repellendus, harum laborum illum at
-      aperiam!
+      <div>
+        <h1>Hello</h1>
+      </div>
     </>
   )
 }
